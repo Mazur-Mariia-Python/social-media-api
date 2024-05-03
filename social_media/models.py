200d9f494ce0d6b709fd32abc23d169c6f371708
ProjectTobi <mariiamazur2927@gmail.com>
2024-05-03 20:14:00 +0300

    Added model Post for social_media app

--- social_media/models.py
+++ social_media/models.py
@@ -46,3 +46,21 @@ class Relationship(models.Model):
 
     def __str__(self):
         return f"{self.my_follower} follows {self.following}"
+
+
+class Post(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    post_picture = models.ImageField(
+        blank=True,
+        null=True,
+        upload_to=post_picture_file_path,
+    )
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE,
+                               related_name="posts")
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Post by {self.author} at {self.created_at}"
