2fef2d78581ef4e4c66a7e7c289eefcf3117471d
ProjectTobi <mariiamazur2927@gmail.com>
2024-05-03 19:59:24 +0300

    Added model Profile for social_media app

--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+    username = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    profile_picture = models.ImageField(
+        blank=True,
+        null=True,
+        upload_to=profile_picture_file_path,
+    )
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
+        verbose_name_plural = "profiles"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
