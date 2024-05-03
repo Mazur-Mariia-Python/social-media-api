b20a4726df9a1e45c58543d54a9ba4ed8af1d40b
ProjectTobi <mariiamazur2927@gmail.com>
2024-05-03 20:51:55 +0300

    Registered models for social_media app in admin panel

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from social_media.models import Profile, Post, Comment, Like, Relationship
+
+admin.site.register(Profile)
+admin.site.register(Relationship)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
